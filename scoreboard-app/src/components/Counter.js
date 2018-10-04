c5bc25539eea9d441dd7f824d57099b2446b0a87
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-04 18:23:16 -0400

    incrementScore this fix

--- scoreboard-app/src/components/Counter.js
+++ scoreboard-app/src/components/Counter.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  constructor() {
-    super();
-    this.state = {
-      score: 0
-    };
+  state = {
+    score: 0
+  };
+
+  incrementScore() {
+    this.setState({
+      score: this.state.score + 1
+    });
   }
   render() {
     return (
       <div className="counter">
         <button className="counter-action decrement"> - </button>
         <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment"> + </button>
+        <button
+          className="counter-action increment"
+          onClick={this.incrementScore.bind(this)}
+        >
+          +
+        </button>
       </div>
     );
   }
