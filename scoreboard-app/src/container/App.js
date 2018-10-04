1ed7f8412e171225b2b9133b07574fc494acee99
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-04 14:06:47 -0400

    import Player and Header

--- scoreboard-app/src/container/App.js
+++ scoreboard-app/src/container/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "../components/Header";
+import Player from "../components/Player";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Player />
       </div>
     );
   }
