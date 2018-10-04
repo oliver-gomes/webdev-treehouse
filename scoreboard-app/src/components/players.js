d55aaf6b27cc8518f99ee1431ccb3edaf013b08f
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-04 17:13:25 -0400

    players object with data

---
+++ scoreboard-app/src/components/players.js
@@ -0,0 +1,18 @@
+const players = [
+  {
+    name: "Harry",
+    score: 50
+  },
+  {
+    name: "Cedric",
+    score: 85
+  },
+  {
+    name: "Krum",
+    score: 95
+  },
+  {
+    name: "Fleur",
+    score: 80
+  }
+];
