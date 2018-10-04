40ffb71c610275a1dbf9716cfd9f12cb4d6e3005
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-04 17:33:39 -0400

    unique id added

--- scoreboard-app/src/components/players.js
+++ scoreboard-app/src/components/players.js
@@ -2,19 +2,23 @@ export default {
   players: [
     {
       name: "Harry",
-      score: 50
+      score: 50,
+      id: 1
     },
     {
       name: "Cedric",
-      score: 85
+      score: 85,
+      id: 2
     },
     {
       name: "Krum",
-      score: 95
+      score: 95,
+      id: 3
     },
     {
       name: "Fleur",
-      score: 80
+      score: 80,
+      id: 4
     }
   ]
 };
