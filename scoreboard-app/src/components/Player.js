76e358a860ec6bf3ed326bd45b938f81c249653b
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-04 14:14:45 -0400

    Counter import

--- scoreboard-app/src/components/Player.js
+++ scoreboard-app/src/components/Player.js
@@ -1,14 +1,11 @@
 import React from "react";
+import Counter from "./Counter";
 
 const Player = () => {
   return (
     <div className="player">
       <span className="player-name">Guil</span>
-      <div className="counter">
-        <button className="counter-action decrement"> - </button>
-        <span className="counter-score" />
-        <button className="counter-action increment"> + </button>
-      </div>
+      <Counter />
     </div>
   );
 };
